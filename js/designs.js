ac1fe11712dbe3a08e7d96e3c11b33ef8592836e
Andrew Buttacavoli <Buttacavoli24@gmail.com>
2018-11-13 13:14:07 -0500

    refractored code

--- js/designs.js
+++ js/designs.js
@@ -1,29 +1,32 @@
-//form sumbit event handler
-$('#gridSize').submit((e) => {
-  //preventing form from resetting
-  e.preventDefault();
-  //removing any applicable classes
-  $("table tr").remove();
-  //rows to create
+function createGrid() {
   let rows = $("#input_height").val();
-  //columns to create
   let cols = $("#input_width").val();
-  //for loop to append the rows and cols to the table
+  $("table tr").remove();
+
   for (var i = 1; i <= rows; i++) {
     $('table').append("<tr></tr>");
-    $('tr').attr("class", 'cells')
+    $('tr').attr("class", 'cells');
+
     for (var j = 1; j <= cols; j++) {
       $("tr:last").append("<td></td>");
       $('td').attr("class", 'cells');
     }
   }
-  // color Selector
-  let paint = $("#colorPicker").val();
+}
+
+//form sumbit event handler
+$('#gridSize').submit((e) => {
+  //preventing form from resetting
+  e.preventDefault();
+  createGrid();
+
   //event handler for painting
   $(".cells").click((e) => {
     e.preventDefault();
+    let paint = $("#colorPicker").val();
     $(e.target).css("background-color", paint);
   });
+
   //event handler for erasing
   $(".cells").dblclick((e) => $(e.target).css("background-color", "#fff"));
 });
